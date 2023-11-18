5a94046b1e78bf747d3c9338aa6eed922745c67d
NazrinNematova <you@example.com>
2023-11-18 12:44:07 +0400

    updated files

--- main.js
+++ main.js
@@ -1,24 +1,81 @@
 const open = document.getElementById("open");
-const close = document.getElementById("close");
-const modal = document.querySelector(".add-modal");
+const closeAdd = document.getElementById("closeAdd");
+const closeUpdate = document.getElementById("closeUpdate");
+const addModal = document.querySelector(".add-modal");
+const updateModal = document.querySelector(".update-modal");
+const addBtn = document.getElementById("add");
+const updateBtn = document.getElementById("update");
+const url = "https://northwind.vercel.app/api/suppliers";
+const companyName = document.getElementById("companyName");
+const contactTitle = document.getElementById("contactTitle");
+const contactName = document.getElementById("contactName");
 
+const updateCompanyName = document.getElementById("updateCompanyName");
+const updateContactTitle = document.getElementById("updateContactTitle");
+const updateContactName = document.getElementById("updateContactName");
+let globalId; 
 open.addEventListener("click", () => {
-    modal.style.display = "flex";
+    addModal.style.display = "flex";
 })
 
-close.addEventListener("click", () => {
-    modal.style.display = "none";
+closeAdd.addEventListener("click", () => {
+    addModal.style.display = "none";
 })
 
-tableRender ()
-function tableRender () {
+closeUpdate.addEventListener("click", () => {
+    updateModal.style.display = "none";
+})
+addBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    fetch(url, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            CompanyName: companyName.value,
+            ContactName: contactName.value,
+            ContactTitle: contactTitle.value
+        })
+    }).then(res => res.ok ? alert("Successful") : alert("Failed"))
+        .finally(() => {
+            companyName.value = ""
+            contactTitle.value = ""
+            contactName.value = ""
+            tableRender()
+        })
+})
+
+updateBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    fetch(`${url}/${globalId}`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            companyName: updateCompanyName.value,
+            contactName: updateContactTitle.value,
+            contactTitle: updateContactName.value
+        })
+    }).then(res => res.ok ? alert("Successful") : alert("Failed"))
+        .finally(() => {
+            updateCompanyName.value = ""
+            updateContactTitle.value = ""
+            updateContactName.value = ""
+            tableRender()
+        })
+})
+
+tableRender()
+function tableRender() {
     const tbodyElem = document.querySelector("tbody")
-    fetch ("https://northwind.vercel.app/api/suppliers")
-    .then(res => res.json())
-    .then(data => {
-        tbodyElem.innerHTML = ""
-        data.forEach(element => {
-            tbodyElem.innerHTML += `
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            tbodyElem.innerHTML = ""
+            data.forEach(element => {
+                tbodyElem.innerHTML += `
             <tr>
             <td scope="row">${element.id}</td>
             <td>${element.companyName}</td>
@@ -26,35 +83,34 @@ function tableRender () {
             <td>${element.contactTitle}</td>
             <td>
                 <button class="btn btn-danger" onclick="deleteUser(${element.id})">delete</button>
-                <button class="btn btn-secondary">update</button>
+                <button class="btn btn-secondary" onclick="updateUser(${element.id})">update</button>
             </td>
         </tr>
             `
-        });
-    })
+            });
+        })
 }
 
 function deleteUser(id) {
-    fetch(`https://northwind.vercel.app/api/suppliers/${id}`, {
+    fetch(`${url}/${id}`, {
         method: "DELETE"
     })
         .finally(() => {
-            console.log("SON");
+            console.log("Deleted!");
             tableRender()
         })
 }
 
-function updateUser (data) {
-    fetch(`https://northwind.vercel.app/api/suppliers/${data}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({})
 
-    })
-       .finally (() => {
-            console.log("SON");
-            tableRender()
-        }) 
-    }
+
+function updateUser(id) {
+    globalId = id 
+    fetch(`${url}/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            updateCompanyName.value = data.companyName
+            updateContactTitle.value = data.contactTitle
+            updateContactName.value = data.contactName
+        })
+    updateModal.style.display = "flex"
+}
